d7470271a67c52d301dc001b72fefd77a876f957
commandcenter <upamanyu.site@gmail.com>
2026-04-17 21:29:18 +0530

    feat: Update configuration for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,9 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
+const isGitHubPages = process.env.GITHUB_PAGES === "true";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: isProd ? "/Portfolio-UpamanyuSamal" : "",
+  ...(isGitHubPages && { output: "export", basePath: "/Portfolio-UpamanyuSamal" }),
   trailingSlash: true,
   compress: true,
   poweredByHeader: false,
